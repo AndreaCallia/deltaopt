1c30b3b8ea77e4209c9436c45b6ff1c8e7c639fe
ac4014 <28895030+AndreaCallia@users.noreply.github.com>
2020-04-16 17:45:22 +0200

    Added objective function verification.

--- verify.py
+++ verify.py
@@ -4,12 +4,31 @@ import sys
 from sympy import sympify
 
 
+objkeyword = 'Obj: '
+
 def error(line, constring):
   print "Error, a constraint does not evaluate as true."
   print "Line: " + str(line)
   print "  Constraint: " + constring
 
+def check_objective(objstring, model):
+  #print "Going to parse: " + objstring
+  obj = sympify(objstring)
+  error = False
+  if (not hasattr(obj.subs(model), 'is_real')):
+    print "Error: The objective value not a legal expression."
+    error = True
+  if (obj.subs(model).is_real != True):
+    print "Error: The objective value is not a real value."
+    error = True
+  if (error):
+    print "Unevaluated objective: " + str(obj)
+    print "Evaluated objective: " + str(obj.subs(model))
+  return (not error)
+
 def main():
+  global objkeyword
+  
   problemfile=open(sys.argv[1], "r")
   problemlines = problemfile.read().splitlines()
   problemfile.close()
@@ -20,23 +39,25 @@ def main():
   
   satisfied = True
   
-  for line_, constring in enumerate(problemlines):
-    line = line_ + 1 
+  for line, constring in enumerate(problemlines, 1):
     if (constring != ""):
-      constraint = sympify(constring)
-      if (not hasattr(constraint, 'subs')):
-        if (constraint != True):
+      if (constring.startswith('Obj: ')):
+        satisfied = check_objective(constring[len(objkeyword):], model)
+      else:
+        constraint = sympify(constring)
+        if (not hasattr(constraint, 'subs')):
+          if (constraint != True):
+            error(line, constring)
+            print "Constraint value: " + str(constraint)
+            if (hasattr(constraint, 'subs')):
+              print "Value after substitution: " + str(constraint.subs(model))
+            satisfied = False
+        elif (constraint.subs(model) != True):
           error(line, constring)
-          print "Constraint value: " + str(constraint)
-          if (hasattr(constraint, 'subs')):
-            print "Value after substitution: " + str(constraint.subs(model))
+          print "  LHS Value: " + str(constraint.lhs.subs(model))
+          print "  RHS Value: " + str(constraint.rhs.subs(model))
+          print "  Constraint value: " + str(constraint.subs(model))
           satisfied = False
-      elif (constraint.subs(model) != True):
-        error(line, constring)
-        print "  LHS Value: " + str(constraint.lhs.subs(model))
-        print "  RHS Value: " + str(constraint.rhs.subs(model))
-        print "  Constraint value: " + str(constraint.subs(model))
-        satisfied = False
 
   if (satisfied == True):
     print "All constraints are satisfied by the model."
